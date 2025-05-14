d36e6fff127b76ab50526132786246da19d4ea8f
zUlisez <zperplexz@gmail.com>
2025-05-14 14:43:18 -0600

    functional

--- repositories/alumno_repository.py
+++ repositories/alumno_repository.py
@@ -9,11 +9,16 @@ class StudentRepository(BaseRepository):
         if row:
             return Student(*row)
         
-    def get_by_career(self, career): # >= 1
+    def get_by_career(self, career) ->list :
         query = 'SELECT * FROM alumnos WHERE career = %s'
         rows = self.get_all(query, (career))
         return [Student(*row) for row in rows ]
             
+    def get_approved(self)-> list:  #alumnos con promedio aprovatorio( >= 6)
+        pass
     
-        
-        
+    def get_failed(self)-> list: #alumnos con promedio reprovatorio ( < 6)
+        pass
+    
+    def get_excellent(self)->list: #alumnos con promedio de excelencia( > 9.4 ) 
+        pass
