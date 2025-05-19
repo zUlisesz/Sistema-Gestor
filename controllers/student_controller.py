833592645031c6098c0ba5cd49e6cc0e2feb6a11
zUlisez <zperplexz@gmail.com>
2025-05-19 09:16:24 -0600

    commit

--- controllers/student_controller.py
+++ controllers/student_controller.py
@@ -1,7 +1,14 @@
 from repositories.student_repository import StudentRepository
-
+from repositories.studentCouse_repository import StudentCourseRepository
 class StudentController:
     
     def __init__(self):
-        self.studen_repository = StudentRepository()
+        self.student_repository = StudentRepository()
+        self.student_course = StudentCourseRepository()
+        
+    def enter_to_course(self, id_student ,id_course):
+        self.student_course.register_student(id_student, id_course)
+        
+    
+        
         
